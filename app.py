0fda61f8b2d4e304199fb72c9690e82cc155e9f9
chaitanyaphalak <chaitanya.phalak@gmail.com>
2017-10-04 23:21:49 -0700

    First serverless app using aws lambda/chalice/python

---
+++ app.py
@@ -0,0 +1,43 @@
+from chalice import Chalice
+from chalice import BadRequestError
+
+app = Chalice(app_name='helloworld')
+
+CITIES_TO_STATE = {
+    'seattle': 'WA',
+    'portland': 'OR',
+}
+
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+@app.route('/cities/{city}')
+def state_of_city(city):
+    try:
+        return {'state': CITIES_TO_STATE[city]}
+    except KeyError:
+        raise BadRequestError("Unknown city '%s', valid choices are: %s" % (
+            city, ', '.join(CITIES_TO_STATE.keys())))
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
